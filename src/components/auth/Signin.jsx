583e61774865ed8470266bd1ac038c6082b0a26c
Muhammad Noman Naeem <nomankallue8560@gmail.com>
2022-09-27 12:48:48 +0500

    initial

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,115 +1,272 @@
-import React from "react";
-import {
-  Form,
-  Button,
-  ButtonToolbar,
-  Schema,
-  FlexboxGrid,
-  Container,
-} from "rsuite";
+import React, { useState } from "react";
 import "./signin.css";
-import { Card, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Col, Row, Form, Button, Container, InputGroup } from "react-bootstrap";
 
-// const JSONView = ({ formValue, formError }) => (
-//   <div style={{ marginBottom: 10 }}>
-//     <Panel className="json-tree-wrapper" header={<p>formValue</p>}>
-//       <JSONTree data={formValue} />
-//     </Panel>
+import { Link, useHistory, Redirect } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+// import {useAlert} from 'react-alert'
+const Signin = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  // const alert = useAlert()
+  const history = useHistory();
+  const [redirect, setRedirect] = useState(false);
+  const [error, setError] = useState("");
+  // const patientdashboard = (data) => {
+  //   if (data.user.type == "Patient") {
+  //     if (data.is_new_registration) {
+  //       console.log(data);
+  //       history.push("/signin/newpatientregistration");
+  //     } else {
+  //       history.push("/all_clinics");
+  //     }
+  //   } else if (data.user.type == "Manager") {
+  //     history.push("/doctor/doctordashboard");
+  //   } else if (data.user.type == "Admin") {
+      // history.push("/manager/addclinic");
+  //   }
+  // };
 
-//     <Panel className="json-tree-wrapper" header={<p>formError</p>}>
-//       <JSONTree data={formError} />
-//     </Panel>
-//   </div>
-// );
-
-const { StringType } = Schema.Types;
-
-const model = Schema.Model({
-  email: StringType()
-    .isEmail("Please enter a valid email address.")
-    .isRequired("This field is required."),
-  password: StringType().isRequired("This field is required."),
-});
-
-const TextField = React.forwardRef((props, ref) => {
-  const { name, label, accepter, ...rest } = props;
-  return (
-    <Form.Group controlId={`${name}-4`} ref={ref}>
-      <Form.ControlLabel>{label} </Form.ControlLabel>
-      <Form.Control name={name} accepter={accepter} {...rest} />
-    </Form.Group>
-  );
-});
-
-const Signup = () => {
-  const formRef = React.useRef();
-  const [formError, setFormError] = React.useState({});
-  const [formValue, setFormValue] = React.useState({
-    email: "",
-    password: "",
-  });
-
-  const handleSubmit = () => {
-    if (!formRef.current.check()) {
-      console.error("Form Error");
-      return;
-    }
-    console.log(formValue, "Form Value");
+  let loginStatusCode;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => {
+        loginStatusCode = res.status;
+        return res.json();
+      })
+      .then((response) => {
+        console.log(response);
+        // setRedirect(true);
+        if (loginStatusCode >= 400 && loginStatusCode <= 499) {
+          if (response.detail) {
+            // setError(response.detail);
+            // alert.error("invalid Email or Password...");
+            history.push("/signin");
+          } else {
+            setError("invalid Credentials");
+            history.push("/signin");
+          }
+        } else {
+          localStorage.setItem("Authorization", `${response.access_token}`);
+          // document.getElementById("log_cre").innerHTML = "";
+          // patientdashboard(jwt_decode(response.access_token));
+          // setError("");
+          // alert.success("Sign In Successfully...");
+          history.push("/");
+          // setRedirect(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        // setError("Network error");
+        // alert.error("Error in Network or server");
+      });
   };
+  // if (redirect) {
+  //   return <Redirect to="/" />;
+  // }
 
   return (
-    <Container>
-      <Row className="justify-content-center">
-        <Col xs={12} md={6} xl={6} className="login">
-          {/* style={{ width: "25rem" }} */}
-          <Card className="shadow e" style={{ width: "27rem" }}>
-            <FlexboxGrid>
-              <FlexboxGrid.Item>
-                <div className="align-items-center d">
-                  <h2 className="mb-0">Sign in</h2>
+    <>
+      <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
+        <Container>
+          {/* <p className="text-center">
+            <Card.Link
+              className="text-muted"
+              as={Link}
+              to={Routes.Landing.path}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to
+              homepage
+            </Card.Link>
+          </p> */}
+          <Row className="justify-content-center">
+            <Col xs={12} md={6} xl={6} className="login">
+              <div
+                className="shadow e  shadow-soft border border-light p-4 p-lg-5 fmxw-500 "
+                style={{ width: "27rem" }}
+              >
+                <div className="text-center text-md-center mb-4 mt-md-0 d">
+                  <h3 className="mb-0">Sign in</h3>
                 </div>
-                <Form
-                  ref={formRef}
-                  onChange={setFormValue}
-                  onCheck={setFormError}
-                  formValue={formValue}
-                  model={model}
-                >
-                  <TextField name="email" label="Email" className="fields" />
-                  <TextField
-                    name="password"
-                    label="Password"
-                    type="password"
-                    autoComplete="off"
-                    className="fields"
-                  />
+                <Form onSubmit={(e) => handleSubmit(e)} className="mt-4">
+                  <Form.Group id="email" className="mb-4 fields">
+                    <Form.Label>Your Email</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        name="Email"
+                        autoFocus
+                        required
+                        type="email"
+                        onChange={(e) => setemail(e.target.value)}
+                      />
+                    </InputGroup>
+                  </Form.Group>
 
-                  <ButtonToolbar className="wrapper">
+                  <Form.Group>
+                    <Form.Group id="password" className="mb-4 fields">
+                      <Form.Label>Your Password</Form.Label>
+                      <InputGroup>
+                        <Form.Control
+                          name="Password"
+                          required
+                          type="password"
+                          onChange={(e) => setpassword(e.target.value)}
+                        />
+                      </InputGroup>
+                    </Form.Group>
+                  </Form.Group>
+                  <div className="d-flex align-items-center justify-content-center wrapper">
                     <Button
-                      appearance="primary"
-                      className="justify-content-center item1"
-                      onClick={handleSubmit}
+                      variant="primary"
+                      type="submit"
+                      className="w-50  item1"
                     >
                       Sign in
                     </Button>
-                  </ButtonToolbar>
-
-                  <div className="d-flex justify-content-center align-items-center mt-4 l">
-                    <span className="fw-normal">
-                      Not registered?{" "}
-                      <Link to="/signup" className="fw-bold aa">
-                        Create Account
-                      </Link>
-                    </span>
                   </div>
                 </Form>
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+
+                {/* <div className="d-flex justify-content-center align-items-center mt-4">
+                  <span className="fw-normal">
+                    Not registered?
+                    <Card.Link
+                      className="fw-bold"
+                      as={Link}
+                      to={Routes.Signup.path}
+                    >
+                      {` Create account `}
+                    </Card.Link>
+                  </span>
+                </div> */}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 };
-export default Signup;
+
+export default Signin;
+
+// import React from "react";
+// import {
+//   Form,
+//   Button,
+//   ButtonToolbar,
+//   Schema,
+//   FlexboxGrid,
+//   Container,
+// } from "rsuite";
+// import "./signin.css";
+// import { Card, Row, Col } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+
+// // const JSONView = ({ formValue, formError }) => (
+// //   <div style={{ marginBottom: 10 }}>
+// //     <Panel className="json-tree-wrapper" header={<p>formValue</p>}>
+// //       <JSONTree data={formValue} />
+// //     </Panel>
+
+// //     <Panel className="json-tree-wrapper" header={<p>formError</p>}>
+// //       <JSONTree data={formError} />
+// //     </Panel>
+// //   </div>
+// // );
+
+// const { StringType } = Schema.Types;
+
+// const model = Schema.Model({
+//   email: StringType()
+//     .isEmail("Please enter a valid email address.")
+//     .isRequired("This field is required."),
+//   password: StringType().isRequired("This field is required."),
+// });
+
+// const TextField = React.forwardRef((props, ref) => {
+//   const { name, label, accepter, ...rest } = props;
+//   return (
+//     <Form.Group controlId={`${name}-4`} ref={ref}>
+//       <Form.ControlLabel>{label} </Form.ControlLabel>
+//       <Form.Control name={name} accepter={accepter} {...rest} />
+//     </Form.Group>
+//   );
+// });
+
+// const Signup = () => {
+//   const formRef = React.useRef();
+//   const [formError, setFormError] = React.useState({});
+//   const [formValue, setFormValue] = React.useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const handleSubmit = () => {
+//     if (!formRef.current.check()) {
+//       console.error("Form Error");
+//       return;
+//     }
+//     console.log(formValue, "Form Value");
+//   };
+
+//   return (
+//     <Container>
+//       <Row className="justify-content-center">
+//         <Col xs={12} md={6} xl={6} className="login">
+//           {/* style={{ width: "25rem" }} */}
+//           <Card className="shadow e" style={{ width: "27rem" }}>
+//             <FlexboxGrid>
+//               <FlexboxGrid.Item>
+//                 <div className="align-items-center d">
+//                   <h2 className="mb-0">Sign in</h2>
+//                 </div>
+//                 <Form
+//                   ref={formRef}
+//                   onChange={setFormValue}
+//                   onCheck={setFormError}
+//                   formValue={formValue}
+//                   model={model}
+//                 >
+//                   <TextField name="email" label="Email" className="fields" />
+//                   <TextField
+//                     name="password"
+//                     label="Password"
+//                     type="password"
+//                     autoComplete="off"
+//                     className="fields"
+//                   />
+
+//                   <ButtonToolbar className="wrapper">
+//                     <Button
+//                       appearance="primary"
+//                       className="justify-content-center item1"
+//                       onClick={handleSubmit}
+//                     >
+//                       Sign in
+//                     </Button>
+//                   </ButtonToolbar>
+
+//                   <div className="d-flex justify-content-center align-items-center mt-4 l">
+//                     <span className="fw-normal">
+//                       Not registered?{" "}
+//                       <Link to="/signup" className="fw-bold aa">
+//                         Create Account
+//                       </Link>
+//                     </span>
+//                   </div>
+//                 </Form>
+//               </FlexboxGrid.Item>
+//             </FlexboxGrid>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+// export default Signup;
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,17 +1,10 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAngleLeft,
-  faEnvelope,
-  faUnlockAlt,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
 // import {useAlert} from 'react-alert'
+import "./signup.css";
 import {
   Col,
   Row,
   Form,
-  Card,
   Button,
   Container,
   InputGroup,
@@ -76,7 +69,7 @@ const Signup = () => {
       <section
         className=" my-5 mt-lg-6 mb-lg-5"
       >
-        <Container className="o">
+        <Container>
           <Row className="justify-content-center">
             <Col xs={12} md={6} xl={6} className="login">
               <div className="shadow e  shadow-soft border border-light p-4 p-lg-5 fmxw-500 " style={{ width: "30rem" }}>
@@ -87,9 +80,6 @@ const Signup = () => {
                   <Form.Group id="email" className="mb-4 fields">
                     <Form.Label>Your Email</Form.Label>
                     <InputGroup>
-                      {/* <InputGroup.Text name="Email">
-                        <FontAwesomeIcon icon={faEnvelope} />
-                      </InputGroup.Text> */}
                       <Form.Control
                         autoFocus
                         required
@@ -104,9 +94,6 @@ const Signup = () => {
                   <Form.Group id="username" className="mb-4 fields">
                     <Form.Label>Your username</Form.Label>
                     <InputGroup>
-                      {/* <InputGroup.Text name="UserName">
-                        <FontAwesomeIcon icon={faUser} />
-                      </InputGroup.Text> */}
                       <Form.Control
                         autoFocus
                         name="UserName"
@@ -121,9 +108,6 @@ const Signup = () => {
                   <Form.Group id="phone_number" className="mb-4 fields">
                     <Form.Label>Phone</Form.Label>
                     <InputGroup>
-                      {/* <InputGroup.Text name="phone_number">
-                        <FontAwesomeIcon icon={faUser} />
-                      </InputGroup.Text> */}
                       <Form.Control
                         autoFocus
                         name="phone_number"
@@ -138,9 +122,6 @@ const Signup = () => {
                   <Form.Group id="password" className="mb-4 fields">
                     <Form.Label>Your Password</Form.Label>
                     <InputGroup>
-                      {/* <InputGroup.Text>
-                        <FontAwesomeIcon icon={faUnlockAlt} />
-                      </InputGroup.Text> */}
                       <Form.Control
                         required
                         type="password"
@@ -158,9 +139,7 @@ const Signup = () => {
                   >
                     <Form.Label>Confirm Password</Form.Label>
                     <InputGroup>
-                      {/* <InputGroup.Text>
-                        <FontAwesomeIcon icon={faUnlockAlt} />
-                      </InputGroup.Text> */}
+
                       <Form.Control
                         name="cpassword"
                         required
@@ -174,13 +153,7 @@ const Signup = () => {
                     </div>
                   </Form.Group>
 
-                  {/* <Form.Group
-                    id="confirmPassword"
-                    name="cpassword"
-                    className="mb-4"
-                  ></Form.Group> */}
-
-                  <div className="d-flex align-items-center justify-content-center wrapper ">
+                  <div className="d-flex align-items-center justify-content-center wrapper">
                     <Button
                       variant="primary"
                       type="submit"
@@ -214,204 +187,3 @@ const Signup = () => {
 
 export default Signup;
 
-// import React, { useState } from "react";
-// import {
-//   Form,
-//   Button,
-//   ButtonToolbar,
-//   Schema,
-//   Panel,
-//   FlexboxGrid,
-//   Container,
-// } from "rsuite";
-// import "./signup.css";
-// import { Card, Row, Col } from "react-bootstrap";
-// import { Link, Redirect } from "react-router-dom";
-// import MaskedInput from "react-text-mask";
-// import axios from "axios";
-
-// const { StringType, NumberType } = Schema.Types;
-
-// const model = Schema.Model({
-//   username: StringType().isRequired("This field is required."),
-//   email: StringType()
-//     .isEmail("Please enter a valid email address.")
-//     .isRequired("This field is required."),
-//   phone_number: StringType().isRequired("This field is required."),
-
-//   password: StringType().isRequired("This field is required."),
-//   password2: StringType()
-//     .addRule((value, data) => {
-//       console.log(data);
-
-//       if (value !== data.password) {
-//         return false;
-//       }
-
-//       return true;
-//     }, "The two passwords do not match")
-//     .isRequired("This field is required."),
-// });
-
-// const InputMask = React.forwardRef(({ onChange, ...rest }, ref) => (
-//   <MaskedInput
-//     {...rest}
-//     ref={ref}
-//     className="rs-input"
-//     onChange={(event) => {
-//       onChange(event.target.value);
-//     }}
-//   />
-// ));
-
-// const TextField = React.forwardRef((props, ref) => {
-//   const { name, label, accepter, ...rest } = props;
-//   return (
-//     <Form.Group controlId={`${name}-4`} ref={ref}>
-//       <Form.ControlLabel>{label} </Form.ControlLabel>
-//       <Form.Control name={name} accepter={accepter} {...rest} />
-//     </Form.Group>
-//   );
-// });
-
-// const mask = [
-//   "(",
-//   /[1-9]/,
-//   /\d/,
-//   ")",
-//   " ",
-//   /\d/,
-//   /\d/,
-//   /\d/,
-//   "-",
-//   /\d/,
-//   /\d/,
-//   /\d/,
-//   /\d/,
-//   /\d/,
-//   /\d/,
-//   /\d/,
-// ];
-
-// const Signup = () => {
-//   const formRef = React.useRef();
-//   const [formError, setFormError] = React.useState({});
-//   const [formValue, setFormValue] = React.useState({
-//     username: "username",
-//     email: "email",
-//     phone_number: "phone_number",
-//     password: "password",
-//     password2: "password2",
-//   });
-//   const [redirect, setRedirect] = useState(false);
-
-//   // const handleSubmit = () => {
-//   //   if (!formRef.current.check()) {
-//   //     console.error("Form Error");
-//   //     return;
-//   //   }
-//   // };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log("Testing");
-//     console.log(formValue);
-//     console.log("Testing");
-//     axios
-//       .post("http://127.0.0.1:8000/api/register", {
-//         formValue,
-//       })
-//       .then((res) => {
-//         console.log(res);
-//         setRedirect(true);
-//       })
-//       .catch((error) => {
-//         console.log("catch");
-//         console.log(error.response.data);
-//         // let err = "please check the entered fields correctly...";
-//         // if (error.response.data.email) err = error.response.data.email;
-//         // else if (error.response.data.password)
-//         //   err = error.response.data.password;
-//         // else if (error.response.data.username)
-//         //   err = error.response.data.username;
-//         // alert.error(err);
-//       });
-//   };
-//   if (redirect) {
-//     return <Redirect to="/" />;
-//   }
-
-//   return (
-//     <Container>
-//       <Row className="justify-content-center">
-//         <Col xs={12} md={6} xl={6} className="login">
-//           <Card className="shadow e" style={{ width: "30rem" }}>
-//             <FlexboxGrid>
-//               <FlexboxGrid.Item>
-//                 <div className="align-items-center d">
-//                   <h2 className="mb-0">Sign up</h2>
-//                 </div>
-//                 <Form
-//                   ref={formRef}
-//                   onChange={setFormValue}
-//                   onCheck={setFormError}
-//                   formValue={formValue}
-//                   model={model}
-//                   onSubmit={(e) => handleSubmit(e)}
-//                 >
-//                   <TextField
-//                     name="username"
-//                     label="Username"
-//                     className="fields"
-//                   />
-//                   <TextField name="email" label="Email" className="fields" />
-//                   <TextField
-//                     name="phone_number"
-//                     label="Phone Number"
-//                     mask={mask}
-//                     accepter={InputMask}
-//                     className="fields"
-//                   />
-//                   <TextField
-//                     name="password"
-//                     label="Password"
-//                     type="password"
-//                     autoComplete="off"
-//                     className="fields"
-//                   />
-//                   <TextField
-//                     name="password2"
-//                     label="Confirm password"
-//                     type="password"
-//                     autoComplete="off"
-//                     className="fields"
-//                   />
-
-//                   <ButtonToolbar className="wrapper">
-//                     <Button
-//                       appearance="primary"
-//                       className="justify-content-center item1"
-//                      type="submit"
-//                     >
-//                       Sign up
-//                     </Button>
-//                   </ButtonToolbar>
-
-//                   <div className="d-flex justify-content-center align-items-center mt-4 l">
-//                     <span className="fw-normal">
-//                       Already have Account?{" "}
-//                       <Link to="/signin" className="fw-bold aa">
-//                         Sign In
-//                       </Link>
-//                     </span>
-//                   </div>
-//                 </Form>
-//               </FlexboxGrid.Item>
-//             </FlexboxGrid>
-//           </Card>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-// export default Signup;
